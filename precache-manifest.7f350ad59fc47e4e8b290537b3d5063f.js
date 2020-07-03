55c121613328889cca1a3f05b20124271176c34d
firoznep <firoz.thekhan@gmail.com>
2020-07-04 02:23:46 +0545

    Updates

---
+++ precache-manifest.7f350ad59fc47e4e8b290537b3d5063f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9a0fd0ff703ecea63a9ef18b4576bd",
+    "url": "/asb-clothing/index.html"
+  },
+  {
+    "revision": "fb7560e26917c51d5a94",
+    "url": "/asb-clothing/static/css/main.9c47d6a0.chunk.css"
+  },
+  {
+    "revision": "60245070fa81bbcbae2e",
+    "url": "/asb-clothing/static/js/2.0042713c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/asb-clothing/static/js/2.0042713c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7560e26917c51d5a94",
+    "url": "/asb-clothing/static/js/main.152566b5.chunk.js"
+  },
+  {
+    "revision": "4871589e29715f7b6529",
+    "url": "/asb-clothing/static/js/runtime-main.c411bbb3.js"
+  }
+]);
