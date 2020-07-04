3f08fb6b381394ca6de8e1a666cb4f756b125487
firoznep <firoz.thekhan@gmail.com>
2020-07-05 02:00:07 +0545

    Updates

---
+++ precache-manifest.dd0dd2fd50fe33ea95a27f71683a8bc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8c253c33373003de8fa4de10b1ab54",
+    "url": "/asb-clothing/index.html"
+  },
+  {
+    "revision": "6c2bda232112f7778c1b",
+    "url": "/asb-clothing/static/css/main.cdbe267e.chunk.css"
+  },
+  {
+    "revision": "03cf753ca4da3d075b86",
+    "url": "/asb-clothing/static/js/2.433e313e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/asb-clothing/static/js/2.433e313e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2bda232112f7778c1b",
+    "url": "/asb-clothing/static/js/main.a1c3e3a2.chunk.js"
+  },
+  {
+    "revision": "4871589e29715f7b6529",
+    "url": "/asb-clothing/static/js/runtime-main.c411bbb3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/asb-clothing/precache-manifest.ba56e8fe507912fdb60a493f308b6cc5.js"
+  "/asb-clothing/precache-manifest.dd0dd2fd50fe33ea95a27f71683a8bc2.js"
 );
 
 self.addEventListener('message', (event) => {
